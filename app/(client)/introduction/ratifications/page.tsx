427b436eb73f95bc237b0556ce2713e8d58c5787
aaradj <aradjahangiri84@gmail.com>
2026-06-15 14:57:52 +0330

    push

---
+++ app/(client)/introduction/ratifications/page.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+const DocumentIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21 7V17C21 20 19.5 22 16 22H8C4.5 22 3 20 3 17V7C3 4 4.5 2 8 2H16C19.5 2 21 4 21 7Z"
+      stroke="#64748B"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14.5 4.5V6.5C14.5 7.6 15.4 8.5 16.5 8.5H18.5"
+      stroke="#64748B"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 13H12"
+      stroke="#64748B"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8 17H16"
+      stroke="#64748B"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const BorderIcon = () => (
+  <svg
+    width="2"
+    height="37"
+    viewBox="0 0 2 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line
+      x1="1"
+      y1="1"
+      x2="0.999998"
+      y2="36"
+      stroke="#2563EB"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+const approvalsListItem = [
+  {
+    id: 1,
+    name: "مصوبات هيئت مديره دوره دهم",
+    list: [
+      {
+        id: 11,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 12,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 13,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 14,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "مصوبات هيئت مديره دوره دهم",
+    list: [
+      {
+        id: 21,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 22,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 23,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 24,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "مصوبات هيئت مديره دوره دهم",
+    list: [
+      {
+        id: 31,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 32,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 33,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+      {
+        id: 34,
+        title: "مصوبات صد و سی و هشتمين",
+        creationDate: "1403/08/19",
+      },
+    ],
+  },
+];
+
+export default function Ratifications() {
+  return (
+    <div className="grid grid-cols-3 gap-3 bg-white rounded-2xl p-3 mt-5">
+      {approvalsListItem.map((item) => {
+        return (
+          <div key={item.id} className="flex flex-col gap-5">
+            <h2 className="text-[#334155] font-bold pr-2 text-xl flex items-center gap-3">
+              <BorderIcon />
+              {item.name}
+            </h2>
+            <div className="flex flex-col gap-3">
+              {item.list.map((child) => (
+                <div key={child.id} className="flex items-center gap-1">
+                  <DocumentIcon />
+                  <p className="text-[#334155]">
+                    {child.title} <span>{child.creationDate}</span>
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ app/components/partial/layout/approvals/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { LayoutsInterface } from "@/@types/App/components.type";
+import Footer from "@/app/components/footer";
+import { Header } from "@/app/components/Header";
+
+const ArrowIcon = () => (
+  <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.5249 9.96004L4.2649 6.70004C3.8799 6.31504 3.8799 5.68504 4.2649 5.30004L7.5249 2.04004"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const approvalsList = [
+  {
+    id: 1,
+    name: "مصوبات هيات مديره و هيات رييسه",
+    path: "/ratifications",
+  },
+  {
+    id: 2,
+    name: "جلسات هیات مدیره",
+    path: "/meeting",
+  },
+  {
+    id: 3,
+    name: "ورود و خروج اعضای هیات مدیره",
+    path: "/login-logout",
+  },
+];
+
+export default function ApprovalsLayout({ children }: LayoutsInterface) {
+  return (
+    <div className="flex flex-col  items-center overflow-hidden">
+      <div className="w-full">
+        <Header />
+      </div>
+      <div className="w-full h-full xl:w-[1230px] 2xl:w-[1450px] font-vazir mt- flex   font-vazirflex flex-col">
+        <div className="flex flex-col gap-1 bg-white rounded-tl-2xl rounded-bl-2xl py-3 px-1">
+          {approvalsList.map((item) => {
+            return (
+              <div key={item.id} className={`w-full flex items-center gap-1`}>
+                <span className="text-black">
+                  <ArrowIcon />
+                </span>
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        <main
+          role="main"
+          aria-label="Main content"
+          className="flex flex-col w-full gap-8"
+        >
+          <div className="mb-5 min-h- max-h-fit">{children}</div>
+        </main>
+      </div>
+      <div className="w-full">
+        <Footer />
+      </div>
+    </div>
+  );
+}
